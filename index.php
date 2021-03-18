db282cec256780d572ff1203d5ef5d5653b5c634
banon jonathan <j.banon01@gmail.com>
2021-03-18 20:46:55 +0100

    html/css finished

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/4b70a28ea3.js" crossorigin="anonymous"></script>
     <title>Fireman</title>
     </head>
     <body>
@@ -15,8 +14,10 @@
                 <img id="logo" src="" alt=""/>
                 <p>logo <br> TAGLINE</p>
             </div>
-            <div>
                 <nav>
+                    <div id="burger">
+                        <img id="burger-img" src="./image_firemen/burger.png" alt="burger"/>
+                    </div>
                     <ul id="menu">
                         <li>
                             <a class="a-active">nav_01</a>
@@ -29,10 +30,6 @@
                         </li>
                     </ul>
                 </nav>
-                <div id="burger">
-                    <img src="./image_firemen/burger.png" alt="burger"/>
-                </div>
-            </div>
         </header>
 
     <section id="container_button">
--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu');
 burger.addEventListener('click', () => {
+    if (menu.style.display === 'none'){
+        menu.style.display = 'flex';
+    }else{
+        menu.style.display = 'none';
+    }
 
     }
 )
