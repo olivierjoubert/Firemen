e880aab0f5bbda984f38aaf0951206a5c8d57380
banon jonathan <j.banon01@gmail.com>
2021-03-18 17:49:13 +0100

    change css nav

--- index.php
+++ index.php
@@ -1,30 +1,52 @@
 <!DOCTYPE html>
 <html>
-<head>
+    <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/4b70a28ea3.js" crossorigin="anonymous"></script>
     <title>Fireman</title>
-</head>
-<body>
-    <header>
-        <div id="logo_container">
-            <img id="logo" src="" alt=""/>
-            <p>logo</p>
-        </div>
-    </header>
+    </head>
+    <body>
+
+        <header>
+            <div id="logo_container">
+                <img id="logo" src="" alt=""/>
+                <p>logo <br> TAGLINE</p>
+            </div>
+            <div>
+                <nav>
+                    <ul id="menu">
+                        <li>
+                            <a class="a-active">nav_01</a>
+                        </li>
+                        <li>
+                            <a>nav_02</a>
+                        </li>
+                        <li>
+                            <a>nav_03</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div id="burger">
+                    <img src="./image_firemen/burger.png" alt="burger"/>
+                </div>
+            </div>
+        </header>
 
     <section id="container_button">
-       <div id="button_page_01">
-           <img id="img_button" src="" alt=""/>
-       </div>
+        <div id="button_page_01">
+            <img id="img_button" src="" alt=""/>
+        </div>
     </section>
 
     <footer>
 
     </footer>
+    <script src="./script.js"></script>
 </body>
+
 </html>
 
 
