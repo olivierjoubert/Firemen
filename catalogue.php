2aea8c2bf4d8b86a4cd5dbcd2bf6ad24257d59e0
Matthieu <matthieudejean@hotmail.com>
2021-03-25 10:33:53 +0100

    our team ready

--- catalogue.php
+++ catalogue.php
@@ -3,21 +3,21 @@
 <?php
 require('head.php')
 ?>
-    <body>
-    <div class="whole_page">
-        <?php
-            $bio = 'Bio : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet interdum risus, tempus fermentum turpis. Suspendisse potenti. Fusce vestibulum feugiat dui, in condimentum sapien eleifend sed. Nunc aliquam odio ac porttitor gravida. Curabitur id neque ac nibh tempor vulputate. Donec egestas arcu nec semper convallis.';
+<body>
+<div class="whole_page">
+    <?php
+    $bio = 'Bio : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet interdum risus, tempus fermentum turpis. Suspendisse potenti. Fusce vestibulum feugiat dui, in condimentum sapien eleifend sed. Nunc aliquam odio ac porttitor gravida. Curabitur id neque ac nibh tempor vulputate. Donec egestas arcu nec semper convallis.';
 
-            $ourFireFighters = [
-                ['id' =>1, 'name' => 'Jean Castex', 'age' => 31, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Bon cuisinier','Capitaine','Présentation soignée',]],
-                ['id' =>2, 'name' => 'Teddy Riner', 'age' => 29, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Maitre des arts martiaux','Junior','Tolérance approfondie',]],
-                ['id' =>3, 'name' => 'Mikel Arteta', 'age' => 36, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Agilité acrue','Monté de corde top niveau','Egaux surdimensionné',]],
-                ['id' =>4, 'name' => 'Mike Horn', 'age' => 45, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Sens du devoir','Adaptation en milieu hostile développée','Généreux',]],
-                ['id' =>5, 'name' => 'Jim Carrey', 'age' => 52, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Autodérision','Serviable','Jeux de rôle',]],
-            ];
+    $ourFireFighters = [
+        ['id' =>1, 'name' => 'Jean Castex', 'age' => 31, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Bon cuisinier','Capitaine','Présentation soignée',]],
+        ['id' =>2, 'name' => 'Teddy Riner', 'age' => 29, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Maitre des arts martiaux','Junior','Tolérance approfondie',]],
+        ['id' =>3, 'name' => 'Mikel Arteta', 'age' => 36, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Agilité acrue','Monté de corde top niveau','Egaux surdimensionné',]],
+        ['id' =>4, 'name' => 'Mike Horn', 'age' => 45, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Sens du devoir','Adaptation en milieu hostile développée','Généreux',]],
+        ['id' =>5, 'name' => 'Jim Carrey', 'age' => 52, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Autodérision','Serviable','Jeux de rôle',]],
+    ];
     require ('header.php')
     ?>
-        <div class ="main-container">
+    <div class ="main-container">
         <div class="catalogue-topbar">
             <div></div>
             <div class="dispo">
@@ -34,19 +34,19 @@ require('head.php')
         <div><img  id="to-carou" class="catalogue-icons" src="assets/carousel.png" alt="Carousel"></div>
         <div><img  id="to-mosaic" class="catalogue-icons" src="assets/mosaic.png" alt="Mosaic"></div>
         <div><a href="catalogueFemme.php"></a><img src="assets/women.png" alt="womanButton"></div></div>
+</div>
+
+<div class = "cards">
+    <div class="carou-left">
     </div>
 
-        <div class = "cards">
-            <div class="carou-left">
+    <?php foreach ($ourFireFighters as $fireFighter) : ?>
+        <div class="card-noshow firefighter" id="firefighter<?= $fireFighter['id'] ?>">
+
+            <div class="card-img" style="background-image: url('<?= $fireFighter['picture'] ?> ');">
             </div>
 
-            <?php foreach ($ourFireFighters as $fireFighter) : ?>
-                <div class="card-noshow firefighter" id="firefighter<?= $fireFighter['id'] ?>">
-
-                    <div class="card-img" style="background-image: url('<?= $fireFighter['picture'] ?> ');">
-                    </div>
-
-                    <div class="card-body">
+            <div class="card-body">
                             <span class="Fireman-name"><?= $fireFighter['name'] . '</span>, ' . $fireFighter['age'] . ' ans'; ?>
                              <ul class="card-spec">
                                 <?php foreach ($fireFighter['specialities'] as $speciality) {
@@ -59,20 +59,20 @@ require('head.php')
                                            type="button"
                                            value="Réservez-moi">
                                 </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-            <div class="carou-right">
             </div>
         </div>
-
+    <?php endforeach; ?>
+    <div class="carou-right">
     </div>
-        <?php require('footer.php') ?>
+</div>
+
+</div>
+<?php require('footer.php') ?>
 
 
-        <script src="src/catalogue.js"></script>
-    </div>
-    </body>
+<script src="src/catalogue.js"></script>
+</div>
+</body>
 </html>
 
 
--- ourTeam.php
+++ ourTeam.php
@@ -4,63 +4,44 @@
 require('head.php')
 ?>
 <body>
-
-<h1>Notre équipe au complet</h1>
-<!--<p>Illorum ab nostra erga sint fiat ipse amicos sunt benevolentia quanti tertiam erga ferri nostra
-    nosmet nosmet amicos nos illorum termini alteram eodem termini probo modo quanti nullam video in.</p>-->
-
 <div class="whole_page">
+    <?php
+    require('header.php')
+    ?>
     <div class="main-container">
-
-        <!--<div class="allTeamMembers">
-            <div class="memberCard">
-                <img class="pictureMember" src="assets/pictures-team-member/Olivier-JOUBERT.JPG" alt="Olivier">
-                <h2 class="nameMember">Olivier</h2>
-                <p>Mind Master</p>
+        <h1 class="equipe">Découvrez notre équipe à travers ce carroussel</h1>
+        <!--<p>Illorum ab nostra erga sint fiat ipse amicos sunt benevolentia quanti tertiam erga ferri nostra
+            nosmet nosmet amicos nos illorum termini alteram eodem termini probo modo quanti nullam video in.</p>-->
+        <?php
+        $ourMemberArray = [
+            ['id' =>1,'pictureMemberArray'=>'src="assets/pictures-team-member/Olivier-JOUBERT.JPG"','nameMemberArray'=>'Olivier','bioMemberArray'=>'Mind Master'],
+            ['id' =>2,'pictureMemberArray'=>'src="assets/pictures-team-member/Jonathan-BANON.JPG"','nameMemberArray'=>'Jonathan','bioMemberArray'=>'Leadership'],
+            ['id' =>3,'pictureMemberArray'=>'src="assets/pictures-team-member/Sara-AJANA.JPG" alt="Sara"','nameMemberArray'=>'Sara','bioMemberArray'=>'Think well about everyone'],
+            ['id' =>4,'pictureMemberArray'=>'src="assets/pictures-team-member/Matthieu-DEJEAN.jpg"','nameMemberArray'=>'Matthieu','bioMemberArray'=>'Collabore même avec lui même'],
+        ];
+        ?>
+        <div class="card-container">
+            <div class="arrow arrow-left">
+                <img src="assets/left-arrow-transparent.png" alt="Arrow Left" />
             </div>
-            <div class="memberCard">
-                <img class="pictureMember" src="assets/pictures-team-member/Jonathan-BANON.JPG" alt="Jonathan">
-                <h2 class="nameMember">Jonathan</h2>
-                <p>Leadership</p>
+                <?php foreach ($ourMemberArray as $member) : ?>
+                    <div class="card-noshow membeR" id="membeR<?= $member['id'] ?>">
+                        <div class="memberCard">
+                            <img class="pictureMember" <?= $member['pictureMemberArray'] ?> alt="<?= $member['id'] ?>">
+                            <h2 class="nameMember"><?= $member['nameMemberArray']?></h2>
+                            <p><?= $member['bioMemberArray']?></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <div class="arrow arrow-right">
+                <img src="assets/right-arrow-transparent.png" alt="arrow right"/>
             </div>
-            <div class="memberCard">
-                <img class="pictureMember" src="assets/pictures-team-member/Sara-AJANA.JPG" alt="Sara">
-                <h2 class="nameMember">Sara</h2>
-                <p>Think well about everyone</p>
-            </div>
-            <div class="memberCard">
-                <img class="pictureMember" src="assets/pictures-team-member/Matthieu-DEJEAN.jpg" alt="Matthieu">
-                <h2 class="nameMember">Matthieu</h2>
-                <p>Collabore même avec lui même</p>
-            </div>
-        </div>-->
-
-<?php
-// Tableau multidimentionel, de quatre tableaux avec picture nom et bio en valeurs;
-// Deux boutons du carousel, fleche gauche fleche droite
-$ourMemberArray = [
-        ['pictureMemberArray'=>'src="assets/pictures-team-member/Olivier-JOUBERT.JPG"','nameMemberArray'=>'Olivier','bioMemberArray'=>'Mind Master'],
-        ['pictureMemberArray'=>'src="assets/pictures-team-member/Jonathan-BANON.JPG"','nameMemberArray'=>'Jonathan','bioMemberArray'=>'Leadership'],
-        ['pictureMemberArray'=>'src="assets/pictures-team-member/Sara-AJANA.JPG" alt="Sara"','nameMemberArray'=>'Sara','bioMemberArray'=>'Think well about everyone'],
-        ['pictureMemberArray'=>'src="assets/pictures-team-member/Matthieu-DEJEAN.jpg"','nameMemberArray'=>'Matthieu','bioMemberArray'=>'Collabore même avec lui même'],
-];
-?>
-<div class="carContainer">
-    <div class="arrow-left">
-        <img src="assets/left-arrow-transparent.png" alt="Arrow Left" />
-    </div>
-    <div class="memberCard">
-        <img class="pictureMember" <?php echo $ourMemberArray[3]['pictureMemberArray'] ?> alt="Matthieu">
-        <h2 class="nameMember"><?php echo $ourMemberArray[3]['nameMemberArray']?></h2>
-        <p><?php echo $ourMemberArray[3]['bioMemberArray']?></p>
-    </div>
-    <div class="arrow-right">
-        <img src="assets/right-arrow-transparent.png" alt="arrow right"/>
+        </div>
     </div>
     <?php
     require('footer.php')
     ?>
-<script src="./script.js"></script>
+</div>
+<script src="src/teamMember.js"></script>
 </body>
-
 </html>
