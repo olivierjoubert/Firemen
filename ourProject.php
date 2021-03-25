703001624488635186ee53704abcedf010968eab
Matthieu <matthieudejean@hotmail.com>
2021-03-25 20:01:03 +0100

    page project

--- ourProject.php
+++ ourProject.php
@@ -9,8 +9,18 @@ require('head.php')
     require('header.php')
     ?>
     <div class="main-container">
-        <h1>Le projet</h1>
-        <img class="caserne" src="assets/Caserne.jpg">
+        <h1 class="titre-caserne">Le projet</h1>
+        <div class="img-projet">
+            <div class="down">
+                <h3 class="text-project">Le moral des français est au plus bas</h3>
+                <img class="img-down" src="assets/trend-down.png">
+            </div>
+                <img class="caserne" src="assets/Caserne.jpg">
+            <div class="down">
+                <h3 class="text-project">Le besoin de proximité augmente</h3>
+                <img src="assets/trend-up.png">
+            </div>
+        </div>
     </div>
     <?php
     require('footer.php')
