c6ad678d17723e09e87cac00534b5542b5a0bc7d
Matthieu <matthieudejean@hotmail.com>
2021-03-25 19:06:59 +0100

    add nurse and fireman

--- assets/firefighters.php
+++ assets/firefighters.php
@@ -2,9 +2,9 @@
 $bio = 'Bio : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet interdum risus, tempus fermentum turpis. Suspendisse potenti. Fusce vestibulum feugiat dui, in condimentum sapien eleifend sed. Nunc aliquam odio ac porttitor gravida. Curabitur id neque ac nibh tempor vulputate. Donec egestas arcu nec semper convallis.';
 
 $peoples = [
-    ['id' =>1, 'name' => 'Jean Castex', 'age' => 31, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Bon cuisinier','Capitaine','Présentation soignée',]],
-    ['id' =>2, 'name' => 'Teddy Riner', 'age' => 29, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Maitre des arts martiaux','Junior','Tolérance approfondie',]],
-    ['id' =>3, 'name' => 'Mikel Arteta', 'age' => 36, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Agilité acrue','Monté de corde top niveau','Egaux surdimensionné',]],
-    ['id' =>4, 'name' => 'Mike Horn', 'age' => 45, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Sens du devoir','Adaptation en milieu hostile développée','Généreux',]],
-    ['id' =>5, 'name' => 'Jim Carrey', 'age' => 52, 'picture' => 'https://images.lanouvellerepublique.fr/image/upload/t_1020w/5d1229021f1a90fe7f8b4580.jpg', 'bio' => $bio, 'specialities' => ['Autodérision','Serviable','Jeux de rôle',]],
+    ['id' =>1, 'name' => 'Jean Castex', 'age' => 31, 'picture' => 'assets/image_pompier/Pompier 1.jpg', 'bio' => $bio, 'specialities' => ['Bon cuisinier','Capitaine','Présentation soignée',]],
+    ['id' =>2, 'name' => 'Teddy Riner', 'age' => 29, 'picture' => 'assets/image_pompier/Pompier 5.jpg', 'bio' => $bio, 'specialities' => ['Maitre des arts martiaux','Junior','Tolérance approfondie',]],
+    ['id' =>3, 'name' => 'Mikel Arteta', 'age' => 36, 'picture' => 'assets/image_pompier/Pompier 3.jpg', 'bio' => $bio, 'specialities' => ['Agilité acrue','Monté de corde top niveau','Egaux surdimensionné',]],
+    ['id' =>4, 'name' => 'Mike Horn', 'age' => 45, 'picture' => 'assets/image_pompier/Pompier 4.jpg', 'bio' => $bio, 'specialities' => ['Sens du devoir','Adaptation en milieu hostile développée','Généreux',]],
+    ['id' =>5, 'name' => 'Jim Carrey', 'age' => 52, 'picture' => 'assets/image_pompier/Pompier 2.jpg', 'bio' => $bio, 'specialities' => ['Autodérision','Serviable','Jeux de rôle',]],
 ];
---
+++ ourProject.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+require('head.php')
+?>
+<body>
+<div class="whole_page">
+    <?php
+    require('header.php')
+    ?>
+    <div class="main-container">
+        <h1>Le projet</h1>
+        <img class="caserne" src="assets/Caserne.jpg">
+    </div>
+    <?php
+    require('footer.php')
+    ?>
+</div>
+<script src="src/teamMember.js"></script>
+</body>
+</html>
